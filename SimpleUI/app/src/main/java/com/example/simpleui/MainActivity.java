5efac3c48908b4bf871015b233084b1abf2e87a6
tony056 <tony61507@gmail.com>
2015-11-26 21:30:51 +0800

    Got instance of hideCheckBox

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText inputText;
+    private CheckBox hideCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         inputText = (EditText)findViewById(R.id.inputText);
         inputText.setText("1234");
+
+        hideCheckBox = (CheckBox) findViewById(R.id.hideCheckBox);
+        hideCheckBox.setChecked(true);
     }
 
     public void submit(View view){
