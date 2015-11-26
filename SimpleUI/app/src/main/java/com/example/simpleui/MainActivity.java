b9817edd4b43964b8f48c537b4b0386ad0e80cf9
tony056 <tony61507@gmail.com>
2015-11-26 21:04:29 +0800

    Setted the onClick method:submit

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -2,17 +2,24 @@ package com.example.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText = (EditText)findViewById(R.id.inputText);
-        editText.setText("1234");
+        inputText = (EditText)findViewById(R.id.inputText);
+        inputText.setText("1234");
+    }
+
+    public void submit(View view){
+        String text = inputText.getText().toString();
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
     }
 }
