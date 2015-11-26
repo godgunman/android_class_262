888f7095c440452dea5fa0ed5ac56c61e3d37ad0
tony056 <tony61507@gmail.com>
2015-11-26 20:41:37 +0800

    Got instance of the EditText

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText)findViewById(R.id.inputText);
+        editText.setText("1234");
     }
 }
