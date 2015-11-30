6654784990b8147c5b8aba59b47160b489383dd9
tony056 <tony61507@gmail.com>
2015-11-30 20:38:22 +0800

    Added ListView

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -6,15 +6,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText inputText;
     private CheckBox hideCheckBox;
+    private ListView historyListView;
 
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor editor;
@@ -52,6 +55,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         hideCheckBox.setChecked(sharedPreferences.getBoolean("hideCheckBox", false));
+
+        historyListView = (ListView) findViewById(R.id.historyListView);
+        setHistory();
+
+    }
+
+    private void setHistory() {
+        String[] data = new String[]{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        historyListView.setAdapter(adapter);
     }
 
     public void submit(View view){
