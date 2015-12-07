595afd761508c1bf2274cae09c8dc766d38ad91a
ggm <ggm@skymizer.com>
2015-12-07 21:58:48 +0800

    get data from  root linearLayout

--- SimpleUI/app/src/main/java/com/example/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/DrinkMenuActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class DrinkMenuActivity extends AppCompatActivity {
 
@@ -21,5 +23,31 @@ public class DrinkMenuActivity extends AppCompatActivity {
         int number = Integer.parseInt(button.getText().toString());
         number++;
         button.setText(String.valueOf(number));
+    }
+        /*
+
+[{"name": "black tea", "l": 2, "m": 0},
+ {"name": "milk tea", "l": 10, "m":3},
+ {"name": "green tea", "l": 5, "m": 3}]
+    * */
+
+    public void getData() {
+        LinearLayout rootLinearLayout =
+                (LinearLayout) findViewById(R.id.root);
+        int count = rootLinearLayout.getChildCount();
+
+        for (int i = 0 ; i < count - 1; i++) {
+            LinearLayout ll = (LinearLayout)
+                    rootLinearLayout.getChildAt(i);
+
+            TextView drinkNameTextView = (TextView) ll.getChildAt(0);
+            Button lButton = (Button) ll.getChildAt(1);
+            Button mButton = (Button) ll.getChildAt(2);
+
+            String drinkName = drinkNameTextView.getText().toString();
+            int lNumber = Integer.parseInt(lButton.getText().toString());
+            int mNumber = Integer.parseInt(mButton.getText().toString());
+        }
+
     }
 }
