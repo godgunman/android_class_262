7b2e8a8f97afb4b517d5dd3f896e94d545e5fde8
ggm <ggm@skymizer.com>
2015-12-10 20:54:55 +0800

    Use SimpleAdapter.

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -13,12 +13,18 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -81,16 +87,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setHistory() {
-        String[] data = Utils.readFile(this, "history.txt").split("\n");
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        String[] rawData = Utils.readFile(this, "history.txt").split("\n");
+
+        List<Map<String, String>> data = new ArrayList<>();
+
+        for (int i = 0 ; i < rawData.length;i++) {
+            try {
+                JSONObject object = new JSONObject(rawData[i]);
+                String note = object.getString("note");
+                JSONArray array = object.getJSONArray("menu");
+
+                Map<String, String> item = new HashMap<>();
+                item.put("note", note);
+                item.put("drinkNum", "15");
+                item.put("storeInfo", "NTU Store");
+
+                data.add(item);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String[] from = {"note", "drinkNum", "storeInfo"};
+        int[] to = {R.id.note, R.id.drinkNum, R.id.storeInfo};
+
+        SimpleAdapter adapter = new SimpleAdapter(this,
+                data, R.layout.listview_item, from, to);
+
         historyListView.setAdapter(adapter);
     }
-/*
-{
-    "note": "this is a note",
-    "menu": [...]
-}
-*/
+
+    /*
+    {
+        "note": "this is a note",
+        "menu": [...]
+    }
+    */
     public void submit(View view) {
         String text = inputText.getText().toString();
         editor.putString("inputText", text);
@@ -98,10 +130,12 @@ public class MainActivity extends AppCompatActivity {
 
         try {
             JSONObject orderData = new JSONObject();
+            if (menuResult == null)
+                menuResult = "[]";
             JSONArray array = new JSONArray(menuResult);
             orderData.put("note", text);
             orderData.put("menu", array);
-            Utils.writeFile(this, "history.txt", orderData.toString());
+            Utils.writeFile(this, "history.txt", orderData.toString() + "\n");
 
         } catch (JSONException e) {
             e.printStackTrace();
