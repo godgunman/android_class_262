c6a185f39db277771c11a85bb05f082553998974
ggm <ggm@skymizer.com>
2015-12-28 19:39:01 +0800

    Use moveCamera().

--- SimpleUI/app/src/main/java/com/example/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/OrderDetailActivity.java
@@ -17,9 +17,11 @@ import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -79,11 +81,12 @@ public class OrderDetailActivity extends AppCompatActivity {
     class GeoCodingTask extends AsyncTask<String, Void, byte[]> {
 
         private String url;
+        private double[] latLng;
 
         @Override
         protected byte[] doInBackground(String... params) {
             String address = params[0];
-            double[] latLng = Utils.addressToLatLng(address);
+            latLng = Utils.addressToLatLng(address);
             url = Utils.getStaticMapUrl(latLng, 17);
             return Utils.urlToBytes(url);
         }
@@ -95,6 +98,11 @@ public class OrderDetailActivity extends AppCompatActivity {
             Bitmap bm =
                     BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
             staticMapImage.setImageBitmap(bm);
+
+            LatLng storeAddress = new LatLng(latLng[0], latLng[1]);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    storeAddress, 17));
+
         }
     }
 }
